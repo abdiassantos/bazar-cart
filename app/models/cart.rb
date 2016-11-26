94e73dfa9c8451c9b3a3c7b4b97e3447dd406ac6
mrodrigues <mrodrigues.uff@gmail.com>
2016-11-26 21:04:22 -0200

    Calculate total in all carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,6 @@ class Cart < ApplicationRecord
   accepts_nested_attributes_for :cart_items, allow_destroy: true
 
   def total
-    cart_items.map(&:price).sum
+    cart_items.map(&:price).sum.to_money
   end
 end
