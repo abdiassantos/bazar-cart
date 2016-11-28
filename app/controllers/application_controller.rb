ac164c1ff47c11652695dc2b85c61feaa453ed4d
mrodrigues <mrodrigues.uff@gmail.com>
2016-11-28 21:02:18 -0200

    Secure with HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   protect_from_forgery with: :exception
 end
